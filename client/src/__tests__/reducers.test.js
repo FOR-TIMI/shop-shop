4dc1f3cd3e9e2aa9e2efa1adc37930dd03572ec4
FOR-TIMI <timiplayskeys@gmail.com>
2022-12-15 07:21:36 -0500

    added all tests for reducers

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -15,12 +15,32 @@ const initialState = {
     currentCategory: '1'
 }
 
-test('UPDATE_PRODUCTS', () => {
+  test('UPDATE_PRODUCTS', () => {
+      let newState = reducer(initialState, {
+        type: UPDATE_PRODUCTS,
+        products: [{}, {}]
+      });
+    
+      expect(newState.products.length).toBe(2);
+      expect(initialState.products.length).toBe(0);
+    });
+
+  test('UPDATE_CATEGORIES', () => {
+     let newState = reducer( initialState, {
+      type: UPDATE_CATEGORIES,
+      categories: [{},{}]
+     })
+
+     expect(newState.categories.length).toBe(2);
+     expect(initialState.categories.length).toBe(1);
+  })
+
+  test('UPDATE_CURRENT_CATEGORY', () => {
     let newState = reducer(initialState, {
-      type: UPDATE_PRODUCTS,
-      products: [{}, {}]
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: '2'
     });
   
-    expect(newState.products.length).toBe(2);
-    expect(initialState.products.length).toBe(0);
+    expect(newState.currentCategory).toBe('2');
+    expect(initialState.currentCategory).toBe('1');
   });
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -12,6 +12,18 @@ import {
           ...state,
           products: [...action.products],
         };
+    // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
+      case UPDATE_CATEGORIES:
+        return {
+          ...state,
+          categories: [...action.categories]
+        }
+
+      case UPDATE_CURRENT_CATEGORY:
+        return {
+          ...state,
+          currentCategory: action.currentCategory
+        }
   
       // if it's none of these actions, do not update state at all and keep things the same!
       default:
