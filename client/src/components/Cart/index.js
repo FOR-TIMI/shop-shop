1996d4fb67832d5afac114a7f93646218e4f8740
FOR-TIMI <timiplayskeys@gmail.com>
2023-05-19 21:49:21 -0400

    Added calculate total functionality

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -13,6 +13,20 @@ const Cart = () => {
   //To open and close cart
   const toggleCart = () => dispatch({ type: TOGGLE_CART });
 
+  //To calculate total price based on the cart
+  const calculateTotal = () => {
+    const { cart } = state;
+    const price = cart
+      .reduce(
+        (total, currentProduct) =>
+          (total += currentProduct.price * currentProduct.purchaseQuantity),
+        0
+      )
+      .toFixed(2);
+
+    return price;
+  };
+
   if (!state.cartOpen) {
     return (
       <div className="cart-closed" onClick={toggleCart}>
@@ -29,33 +43,28 @@ const Cart = () => {
         [close]
       </div>
       <h2>Shopping Cart</h2>
-      <div>
-        <CartItem
-          item={{
-            name: "Camera",
-            image: "camera.jpg",
-            price: 5,
-            purchaseQuantity: 3,
-          }}
-        />
-        <CartItem
-          item={{
-            name: "Soap",
-            image: "soap.jpg",
-            price: 6,
-            purchaseQuantity: 4,
-          }}
-        />
-
-        <div className="flex-row space-between">
-          <strong>Total: $0</strong>
-          {Auth.loggedIn() ? (
-            <button>Checkout</button>
-          ) : (
-            <span>(log in to check out)</span>
-          )}
+      {state.cart.length ? (
+        <div>
+          {state.cart.map((item) => (
+            <CartItem key={item._id} item={item} />
+          ))}
+          <div className="flex-row space-between">
+            <strong>Total: ${calculateTotal()}</strong>
+            {Auth.loggedIn() ? (
+              <button>Checkout</button>
+            ) : (
+              <span>(log in to check out)</span>
+            )}
+          </div>
         </div>
-      </div>
+      ) : (
+        <h3>
+          <span role="img" aria-label="shocked">
+            😱
+          </span>
+          You haven't added anything to your cart yet!
+        </h3>
+      )}
     </div>
   );
 };
--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,30 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { pluralize } from "../../utils/helpers"
+import { useStoreContext } from "../../utils/GlobalState";
+import {
+  ADD_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  UPDATE_PRODUCTS,
+} from "../../utils/actions";
+import { pluralize } from "../../utils/helpers";
 
 function ProductItem(item) {
-  const {
-    image,
-    name,
-    _id,
-    price,
-    quantity
-  } = item;
+  const { image, name, _id, price, quantity } = item;
+
+  const [state, dispatch] = useStoreContext();
+
+  const addToCart = () => {
+    dispatch({
+      type: ADD_TO_CART,
+      product: { ...item, purchaseQuantity: 1 },
+    });
+  };
 
   return (
     <div className="card px-1 py-1">
       <Link to={`/products/${_id}`}>
-        <img
-          alt={name}
-          src={`/images/${image}`}
-        />
+        <img alt={name} src={`/images/${image}`} />
         <p>{name}</p>
       </Link>
       <div>
-        <div>{quantity} {pluralize("item", quantity)} in stock</div>
+        <div>
+          {quantity} {pluralize("item", quantity)} in stock
+        </div>
         <span>${price}</span>
       </div>
-      <button>Add to cart</button>
+      <button onClick={addToCart}>Add to cart</button>
     </div>
   );
 }
