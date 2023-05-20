29f690aceb9619057234ee6ae662cee9699a4d0c
FOR-TIMI <timiplayskeys@gmail.com>
2023-05-20 03:21:27 -0400

    Added succes page and order history page

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import {
   ApolloClient,
-  InMemoryCache,
   ApolloProvider,
+  InMemoryCache,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import React from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
-import Home from './pages/Home';
-import Detail from './pages/Detail';
-import NoMatch from './pages/NoMatch';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Nav from './components/Nav';
-import OrderHistory from './pages/OrderHistory';
+import Nav from "./components/Nav";
+import Detail from "./pages/Detail";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NoMatch from "./pages/NoMatch";
+import OrderHistory from "./pages/OrderHistory";
+import Signup from "./pages/Signup";
+import Success from "./pages/Success";
 
-
-import { StoreProvider } from './utils/GlobalState';
+import { StoreProvider } from "./utils/GlobalState";
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -46,36 +46,13 @@ function App() {
           <StoreProvider>
             <Nav />
             <Switch>
-              <Route 
-                exact
-                path="/" 
-                component={Home } 
-              />
-              <Route 
-                exact
-                path="/login" 
-                component={Login }
-              />
-              <Route 
-                exact
-                path="/signup" 
-                component={Signup } 
-              />
-              <Route 
-                exact
-                path="/orderHistory" 
-                component={OrderHistory } 
-              />
-              <Route 
-                exact
-                path="/products/:id" 
-                component={Detail}
-              />
-              <Route 
-                exact
-                path="*" 
-                component={NoMatch }
-              />
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route path="/success" component={Success} />
+              <Route exact path="/orderHistory" component={OrderHistory} />
+              <Route exact path="/products/:id" component={Detail} />
+              <Route exact path="*" component={NoMatch} />
             </Switch>
           </StoreProvider>
         </div>
