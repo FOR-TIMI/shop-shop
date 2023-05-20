83f40e90f2081c0beaede2de33b4b1b36df83cfe
FOR-TIMI <timiplayskeys@gmail.com>
2023-05-19 21:33:54 -0400

    Update global state to include toggle cart

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -2,12 +2,32 @@ import React from "react";
 import Auth from "../../utils/auth";
 import CartItem from "../CartItem";
 
+import { useStoreContext } from "../../utils/GlobalState";
+import { TOGGLE_CART } from "../../utils/actions";
+
 import "./style.css";
 
 const Cart = () => {
+  const [state, dispatch] = useStoreContext();
+
+  //To open and close cart
+  const toggleCart = () => dispatch({ type: TOGGLE_CART });
+
+  if (!state.cartOpen) {
+    return (
+      <div className="cart-closed" onClick={toggleCart}>
+        <span role="img" aria-label="trash">
+          🛒
+        </span>
+      </div>
+    );
+  }
+
   return (
     <div className="cart">
-      <div className="close">[close]</div>
+      <div className="close" onClick={toggleCart}>
+        [close]
+      </div>
       <h2>Shopping Cart</h2>
       <div>
         <CartItem
